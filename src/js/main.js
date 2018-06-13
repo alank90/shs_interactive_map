55ea2f774336863a9549a75a58c7bfe20ec29ce3
Alan K <akillian@outlook.com>
2018-06-13 14:03:08 -0400

    Deleted  z-index

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ const $ = require("jquery");
 // DOM is ready
 $(document).ready(function() {
   // ============ Routine to reveal/hide layers ======== //
-  $("div").on("click", (event) => {
+  $("div").on("click", "img", (event) => {
     const el = event.target;
     /* var id = el.attr("id"); */
      console.log(el);
