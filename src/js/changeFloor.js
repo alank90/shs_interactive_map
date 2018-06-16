38cca5b0bc7d05d89fd62ec4a13b4fd70f87fb86
akillian@outlook.com <akillian@outlook.com>
2018-06-16 18:09:12 -0400

    commit

--- src/js/changeFloor.js
+++ src/js/changeFloor.js
@@ -1,8 +1,15 @@
-
-const changeFloor = (currentFloor, newFloor) => {
+const $ = require("jquery");
+const CHANGEFLOOR = (currentFloor, newFloor) => {
   console.log("currentFloor " + currentFloor);
-  // Hide Currently Displayed Floor in DOM
-  $(`#${currentfloor}`).addClass("hidden");
+  console.log("newFloor " + newFloor);
+
+  $(`#${currentFloor}`).addClass("hidden"); // // Hide Currently Displayed Floor in DOM
+  let siblings = $(`#${currentFloor}`).siblings();
+  console.log("Siblings are " + siblings);
+
+  // $(`#${currentFloor}`).siblings().addClass("hidden"); // // Hide all currently displayed departments in DOM
+  $(`#${newFloor}`).removeClass("hidden"); // Reveal newFloor
+  return newFloor; // Return newFloor to become currentFllor
 };
 
-module.exports = changeFloor;
+module.exports = CHANGEFLOOR;
--- src/js/main.js
+++ src/js/main.js
@@ -6,20 +6,33 @@ $(document).ready(function() {
   var currentFloor = "floor-one";
 
   // ============ Routine to reveal/hide elements on floor ======== //
-  $(".one").on("click", "li", event => {
-    const el = event.currentTarget;
+  $("ul").on("click", "li", event => {
+    const EL = event.currentTarget;
+    console.log("this EL " + EL);
+    const _this = $(this);
+    console.log("_this is " + _this);
+    let grandparent = _this.parents();
+    console.log("Grandparent is " + grandparent);
+    console.log(Object.getOwnPropertyNames(EL));
 
-    const dept = $(el).data("department");
-    console.log(dept);
-    $(dept).toggleClass("hidden");
+    /* Right here need to check if li element floor that was 
+       clicked on is equal to currentFloor. If not we then need 
+       to call changeFloor method to clear out layout and change
+       currentFloor.
+        */
+
+    const DEPT = $(EL).data("department");
+    console.log(DEPT);
+    $(DEPT).toggleClass("hidden");
   });
 
   // Get the Floor Layout number that was clicked and
   // call showFloorLayout to display clicked floor
-  $(".floors > div").on("click", event => {
-    const el = event.currentTarget;
-    const newFloor = $(el).data("floor");
-    console.log(newFloor);
-    changeFloor(currentFloor, newFloor);
-  });
+  /* $(".floors > div").on("click", event => {
+    const EL = event.currentTarget;
+    let newFloor = $(EL).data("floor");
+    
+    currentFloor = changeFloor(currentFloor, newFloor);
+    console.log("The Current floor is " + currentFloor);
+  }); */
 }); // end dom ready
