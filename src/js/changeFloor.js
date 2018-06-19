d31c8cc5b8c4d3692be4d862fbb866cfc53b47fe
akillian@outlook.com <akillian@outlook.com>
2018-06-19 18:27:55 -0400

    commit

--- src/js/changeFloor.js
+++ src/js/changeFloor.js
@@ -1,15 +1,16 @@
 const $ = require("jquery");
+
+
 const CHANGEFLOOR = (currentFloor, newFloor) => {
-  console.log("currentFloor " + currentFloor);
-  console.log("newFloor " + newFloor);
-
+  
   $(`#${currentFloor}`).addClass("hidden"); // // Hide Currently Displayed Floor in DOM
-  let siblings = $(`#${currentFloor}`).siblings();
-  console.log("Siblings are " + siblings);
-
-  // $(`#${currentFloor}`).siblings().addClass("hidden"); // // Hide all currently displayed departments in DOM
+  let siblings = $(`#${currentFloor}`).siblings(); //Get siblings of div#floor-* 
+  
+  $(`#${currentFloor}`)
+    .siblings()
+    .addClass("hidden"); // Hide all currently displayed departments in DOM
   $(`#${newFloor}`).removeClass("hidden"); // Reveal newFloor
-  return newFloor; // Return newFloor to become currentFllor
+  return newFloor; // Return newFloor to become currentFloor
 };
 
 module.exports = CHANGEFLOOR;
--- src/js/main.js
+++ src/js/main.js
@@ -8,31 +8,28 @@ $(document).ready(function() {
   // ============ Routine to reveal/hide elements on floor ======== //
   $("ul").on("click", "li", event => {
     const EL = event.currentTarget;
-    console.log("this EL " + EL);
-    const _this = $(this);
-    console.log("_this is " + _this);
-    let grandparent = _this.parents();
-    console.log("Grandparent is " + grandparent);
-    console.log(Object.getOwnPropertyNames(EL));
+
+    let newFloor = $(EL)
+      .parents(":eq(1)")
+      .data("floor"); // Grandparent div data-floor value
 
     /* Right here need to check if li element floor that was 
        clicked on is equal to currentFloor. If not we then need 
        to call changeFloor method to clear out layout and change
        currentFloor.
         */
+    if (currentFloor !== newFloor) {
+      currentFloor = changeFloor(currentFloor, newFloor);
+      console.log("The New Current floor is " + currentFloor);
+    }
 
+    // Add dept to onscreen layout
     const DEPT = $(EL).data("department");
-    console.log(DEPT);
+    console.log(`The clicked DEPT is ${DEPT}`);
     $(DEPT).toggleClass("hidden");
-  });
 
-  // Get the Floor Layout number that was clicked and
-  // call showFloorLayout to display clicked floor
-  /* $(".floors > div").on("click", event => {
-    const EL = event.currentTarget;
-    let newFloor = $(EL).data("floor");
-    
-    currentFloor = changeFloor(currentFloor, newFloor);
-    console.log("The Current floor is " + currentFloor);
-  }); */
+    // Toggle the text color for choosen item 
+    let menuItemChoosen = DEPT.substring(1);
+    $(EL).toggleClass(menuItemChoosen);
+  });
 }); // end dom ready
